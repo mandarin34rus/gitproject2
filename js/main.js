e7cbd39c76b9424836154abccd91e843ec652109
VaLi768 <lerapavlova916@gmail.com>
2024-01-14 15:54:40 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -17,7 +17,8 @@ document.querySelector('button.add_new').addEventListener('click', function(e) {
         let goods = JSON.parse(localStorage.getItem('goods'))
         goods.push(['good_'+ goods.length, name, prise, count, 0, 0, 0])
         localStorage.setItem('goods', JSON.stringify(goods))
-      
+        update_goods()
+        
         myModal.hide()
     } else {
         Swal.fire({
@@ -26,3 +27,30 @@ document.querySelector('button.add_new').addEventListener('click', function(e) {
             text: 'пожалуста, заполните все поля!',
         })
     }})
+
+update_goods()
+
+function update_goods(){
+    let tbody = document.querySelector('.list')
+    tbody.innerHTML = ""
+    document.querySelector('.cart').innerHTML = ""
+    let goods = JSON.parse(localStorage.getItem('goods'))
+    if (goods.length){
+        // table1.hidden = false
+        // table1.hidden = false
+        for (let i=0; i < goods.length; i++){// python i +=1
+            tbody.insertAdjacentHTML('beforeend',
+            `
+            <tr class='alighn-middle'>
+            <td>${i+1}</td>
+            <td class ='name'>${goods[i][1]}</td>
+            <td class ='prise'>${goods[i][2]}</td>
+            <td>${goods[i][3]}</td>
+            <td><button class ='good_delete btn-danger' data-delete='${goods[i][0]}'>&#10006;</button></td>
+            <td><button class ='good_delete btn-primary' data-goods='${goods[i][0]}'>&#10149;</button></td>
+            </tr>
+            `
+            )
+        }
+    }
+}
